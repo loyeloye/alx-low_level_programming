a33014438a475350a47d09f777a6f546880287d8
loyeloye <davidakin6@gmail.com>
2021-02-09 22:44:35 +0000

    24 hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -21,7 +21,7 @@ for (min = 0; min < 60; min++)
 _putchar('0' + (hours / 10));
 _putchar('0' + (hours % 10));
 _putchar(':');
-_putchat('0' + (min / 10));
+_putchar('0' + (min / 10));
 _putchar('0' + (min % 10));
 _putchar('\n');
 
