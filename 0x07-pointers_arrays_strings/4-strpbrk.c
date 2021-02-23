522f15643d48676f9c46a0d99bb2963d56fa578a
loyeloye <davidakin6@gmail.com>
2021-02-23 13:55:31 +0000

    Searches Strings

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#include <stddef.h>
+/**
+ * _strpbrk - searches a string for bytes
+ * @s: the string
+ * @accept: set of bytes
+ * Return: a pointer to the byte in s that matches bytes in accept
+ */
+char *_strpbrk(char *s, char *accept)
+{
+unsigned int i;
+unsigned int j;
+
+for (i = 0; s[i] != '\0'; i++)
+{
+for (j = 0; accept[j] != '\0'; j++)
+{
+if (s[i] == accept[j])
+return (&s[i]);
+}
+}
+return (NULL);
+}
